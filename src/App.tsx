aee05aaba68b48224ab05716e0e192f916667146
Vitaliy <shulikvitalya@gmail.com>
2021-01-24 16:27:44 +0600

    generate testnet address

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import * as bitcoin from 'bitcoinjs-lib';
+
+const TESTNET = bitcoin.networks.testnet;
+
 
 const App: React.FC = () => {
+  const [pubAddress, setPubAddress] = useState('');
+
+  const generateAddress = () => {
+    const keyPair = bitcoin.ECPair.makeRandom({ network: TESTNET });
+    const { address } = bitcoin.payments.p2pkh({
+      pubkey: keyPair.publicKey,
+      network: TESTNET,
+    });
+    return address;
+  }
+
+  useEffect(() => {
+    const saved = localStorage.getItem('pubAddress') || generateAddress();
+    console.log(saved);
+    setPubAddress(saved);
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem('pubAddress', pubAddress)
+  }, [pubAddress])
+
+
   return (
     <div>
-      Test
+      {pubAddress}
     </div> 
   );
 }
