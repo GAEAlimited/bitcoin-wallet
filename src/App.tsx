b30a668dd7758656fbb02ba427c932be0d1db793
Vitaliy <shulikvitalya@gmail.com>
2021-01-24 17:09:23 +0600

    add get address api

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import * as bitcoin from 'bitcoinjs-lib';
+import * as axiosDef from "axios";
 
+const axios = axiosDef.default;
 const TESTNET = bitcoin.networks.testnet;
+const TESTNET_API_ADDRESS = 'https://api.blockcypher.com/v1/btc/test3/addrs/';
 
 
 const App: React.FC = () => {
   const [pubAddress, setPubAddress] = useState('');
+  const [balance, setBalance] = useState(0);
 
   const generateAddress = () => {
     const keyPair = bitcoin.ECPair.makeRandom({ network: TESTNET });
@@ -16,10 +20,23 @@ const App: React.FC = () => {
     return address;
   }
 
+  const getBalance = (pubAddress: string) => {
+    axios.get(`${TESTNET_API_ADDRESS}${pubAddress}/full`, {
+      params: {
+        limit: 50
+      }
+    }).then(response => {
+      const balance = response.data.balance / 10**8;
+      setBalance(balance)
+    }).catch(error => {
+      console.log(error);
+    })
+  }
+
   useEffect(() => {
-    const saved = localStorage.getItem('pubAddress') || generateAddress();
-    console.log(saved);
-    setPubAddress(saved);
+    const savedAddress = localStorage.getItem('pubAddress') || generateAddress();
+    getBalance(savedAddress);
+    setPubAddress(savedAddress);
   }, [])
 
   useEffect(() => {
@@ -29,7 +46,8 @@ const App: React.FC = () => {
 
   return (
     <div>
-      {pubAddress}
+      <p>Address: {pubAddress}</p>
+      <p>Balance: {balance} BTC</p>
     </div> 
   );
 }
